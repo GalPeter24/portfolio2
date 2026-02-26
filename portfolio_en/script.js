f999877dcadce4e1c33db69f441853eef9c35d0a
GalPeter24 <galpeti20083@gmail.com>
2026-02-26 19:47:41 +0100

    02.26

--- portfolio_en/script.js
+++ portfolio_en/script.js
@@ -14,3 +14,29 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slider = document.getElementById('slider');
+    const totalCards = document.querySelectorAll('.project__card').length;
+
+    currentIndex += direction;
+
+    // Ha túlmennénk az elején, ugorjon az utolsóra
+    if (currentIndex < 0) {
+        currentIndex = totalCards - 1;
+    } 
+    // Ha túlmennénk a végén, ugorjon az elsőre
+    else if (currentIndex >= totalCards) {
+        currentIndex = 0;
+    }
+
+    const offset = currentIndex * -100;
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+function sendMessage(){
+    window.alert("Message sent")
+}
